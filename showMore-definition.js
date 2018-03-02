00d7e978ab05aa3b3e29824782a9d48425b11938
JimmySevere <JimmySevere@users.noreply.github.com>
2018-03-02 16:50:10 +0000

    Create showMore-definition.js

---
+++ showMore-definition.js
@@ -0,0 +1,55 @@
+//showMore - overflow adjustment plugin
+{
+  name:'showMore',
+  init:function(opts){
+    var $btn = $(this);
+    var target = $(opts.target).get(0);
+    if(target.clientHeight >= target.scrollHeight){
+      $btn.hide();
+    }
+    $btn.data('text', $btn.text());
+    $btn.on(opts.trigger, function(e){
+      if(opts.preventDefault){
+        e.preventDefault();
+      }
+      $(this).showMore('toggle');
+    });
+  },
+  actionEach:function(action, opts){
+    var $target = $(opts.target);
+    var target = $target.get(0);
+    var $btn = $(this);
+    var hasAlt = typeof opts.altText == 'string' && opts.altText;
+
+    switch(action){
+      case 'show':
+        $target.height(target.scrollHeight);
+        $target.add($btn).addClass('showing-more');
+        if(hasAlt){
+          $btn.text(opts.altText);
+        }
+      break;
+      case 'hide':
+        if(opts.canHide){
+          $target.css('height', '');
+          $target.add($btn).removeClass('showing-more');
+          if(hasAlt){
+            $btn.text($btn.data('text'));
+          }
+        }
+      break;
+      case 'toggle':
+        if(target.clientHeight < target.scrollHeight){
+          $btn.showMore('show');
+        }else{
+          $btn.showMore('hide');
+        }
+      break;
+    }
+  },
+  defaults:{
+    trigger:'click',
+    canHide:true,
+    preventDefault:true
+  }
+}
