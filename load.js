238955f93509ca1e142a964a01aa40ededd3753e
JimmySevere <JimmySevere@users.noreply.github.com>
2018-03-20 16:37:50 +0000

    better min, add bind data fns by default

--- load.js
+++ load.js
@@ -2,16 +2,17 @@
   'use strict';
   
   /**
-	 * function bopJQPlugin
-	 * 
-	 * 
-	 * bsJQPlugin(plugin [, ...plugins])
-	 * 
-	 * @param object plugin - the object definition of the plugin
-	 * 	(required)
-	 * @param objects plugins - arbitrary no. of further plugin defintions
-	 * 	(optional)
-	 * @return null
+   * function bopJQPlugin
+   * 
+   * version: 1.0.1
+   * 
+   * bsJQPlugin(plugin [, ...plugins])
+   * 
+   * @param object plugin - the object definition of the plugin
+   * 	(required)
+   * @param objects plugins - arbitrary no. of further plugin defintions
+   * 	(optional)
+   * @return null
    * 
    * @object plugin
    * {
@@ -43,25 +44,30 @@
     //set jquery function
     $.fn[plugin.name] = function(optsOrAction){
       
+      var obTypeStr = 'object',
+        undefTypeStr = 'undefined',
+        fnTypeStr = 'function',
+        strTypeStr = 'string';
+      
       //set options
-      if(typeof optsOrAction == 'object'){
+      if(typeof optsOrAction == obTypeStr){
         var opts = optsOrAction;
         
         var defaults = {};
-        if(typeof plugin.defaults == 'object'){
+        if(typeof plugin.defaults == obTypeStr){
           defaults = plugin.defaults;
         }
         
         opts = $.extend({}, defaults, opts);
         this.each(function(){
-          if(typeof this.bopOpts == 'undefined'){
+          if(typeof this.bopOpts == undefTypeStr){
             this.bopOpts = {};
           }
           this.bopOpts[plugin.name] = opts;
         });
         
         //run init
-        if(typeof plugin.init == 'function'){
+        if(typeof plugin.init == fnTypeStr){
           var initFn = plugin.init.bind(this);
           return initFn(opts, plugin);
         }
@@ -72,19 +78,19 @@
       //ooo principle
       function getOpts(el){
         var opts = {};
-        if(typeof el != 'undefined' && typeof el.bopOpts != 'undefined' && typeof el.bopOpts[plugin.name] != 'undefined'){
+        if(typeof el != undefTypeStr && typeof el.bopOpts != undefTypeStr && typeof el.bopOpts[plugin.name] != undefTypeStr){
           opts = el.bopOpts[plugin.name];
         }
         return opts;
       }
       
       //run action
-      if(typeof optsOrAction == 'string'){
+      if(typeof optsOrAction == strTypeStr){
         var action = optsOrAction;
-        if(typeof plugin.action == 'function'){
+        if(typeof plugin.action == fnTypeStr){
           var actionFn = plugin.action.bind(this);
           return actionFn(action, getOpts(this.get(0)), plugin);
-        }else if(typeof plugin.actionEach == 'function'){
+        }else if(typeof plugin.actionEach == fnTypeStr){
           this.each(function(){
             var actionFn = plugin.actionEach.bind(this);
             actionFn(action, getOpts(this), plugin);
@@ -94,21 +100,29 @@
       }
       
       //run with no args
-      if(typeof optsOrAction == 'undefined' && typeof plugin.run == 'function'){
+      if(typeof optsOrAction == undefTypeStr && typeof plugin.run == fnTypeStr){
         var runFn = plugin.run.bind(this);
         return runFn(getOpts(this.get(0)), plugin);
       }
     };
   }
   
+  bopJQPlugin(
+    {
+      name:'bindBopDataFns',
+      run:function(){
+        this.find('[data-bop-fn]').each(function(){
+          var $this = $(this);
+          var fn = $this.data('bop-fn');
+          if(typeof $this[fn] == 'function'){
+            $this[fn]($this.data());
+          }
+        });
+      }
+    }
+  );
   //init data functions
   $(document).ready(function(){
-    $('[data-bop-fn]').each(function(){
-      var $this = $(this);
-      var fn = $this.data('bop-fn');
-      if(typeof $this[fn] == 'function'){
-        $this[fn]($this.data());
-      }
-    });
+    $(this).bindBopDataFns();
   });
 })(jQuery);
