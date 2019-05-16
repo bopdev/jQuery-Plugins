0faf68e59b4417473f6a158dd4a59ec5a3dfba04
JimmySevere <JimmySevere@users.noreply.github.com>
2019-05-16 15:54:28 +0000

    Method of getting options makes more sense.

--- plugin-loader.js
+++ plugin-loader.js
@@ -118,7 +118,7 @@
           var $this = $(this);
           var fn = $this.data('bop-fn');
           if(typeof $this[fn] == 'function'){
-            $this[fn]($this.data());
+            $this[fn]($this.data(fn+'-opts'));
           }
         });
       }
